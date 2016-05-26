437df787fa36f9d03f1d4f48b7f5472907d2439f
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2016-05-27 01:07:36 +0200

    Share version between projects and bump to 1.0.1.0

---
+++ VersionInfo.cs
@@ -0,0 +1,10 @@
+using System.Reflection;
+
+
+[assembly: AssemblyVersion(VersionInfo.Version)]
+[assembly: AssemblyFileVersion(VersionInfo.Version)]
+
+class VersionInfo
+{
+    public const string Version = "1.0.1.0";
+}
